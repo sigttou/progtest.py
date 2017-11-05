a076f7d8918d0d0b10515a0b91618bc5180ccd72
David Bidner <david.bidner@student.tugraz.at>
2017-11-05 09:37:07 +0100

    CHG: copyright year

--- progtest.py
+++ progtest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Copyright (c) 2015, 2016 David Bidner <david -at- crap.solutions>
+Copyright (c) 2015 - 2017, David Bidner <david -at- crap.solutions>
 
 Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted,
 provided that the above copyright notice and this permission notice appear in all copies.
